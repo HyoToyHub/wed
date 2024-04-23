28b9825b07dc4532ced42741b0ad5d34a370b1bd
hyo <kdhyo98@gmail.com>
2024-04-23 22:42:46 +0900

    백그라운드 이미지 변경 이벤트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -27,19 +27,30 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  // 기준 날짜 설정
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   static final DateTime anniversary = DateTime(2023, 4, 25);
-
-  // 날짜 차이 계산
-  // static final int daysDifference = today.difference(anniversary).inDays;
-
   int dDayText = -1;
+
+  static const String _currentBackground = 'assets/images/background-01.jpeg';
+  static const String _nextBackground = 'assets/images/background-02.jpeg';
+  bool _isFirstImageShown = true;
+
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
 
   @override
   void initState() {
     super.initState();
     updateDDay();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _opacityAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(_controller)
+      ..addListener(() {
+        setState(() {}); // Trigger rebuild whenever animation value changes
+      });
   }
 
   void updateDDay() {
@@ -49,54 +60,90 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void toggleBackground() {
+    if (_isFirstImageShown) {
+      _controller.forward(); // Fade out the first image
+    } else {
+      _controller.reverse(); // Fade in the first image
+    }
+    _isFirstImageShown = !_isFirstImageShown;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(fit: StackFit.expand, children: <Widget>[
-        Image.asset(
-          'assets/images/background-01.jpeg',
-          fit: BoxFit.cover,
+      body: GestureDetector(
+        onTap: toggleBackground,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              _currentBackground,
+              fit: BoxFit.cover,
+              key: const Key(_currentBackground),
+            ),
+            FadeTransition(
+              opacity: _opacityAnimation,
+              child: Image.asset(
+                _nextBackground,
+                fit: BoxFit.cover,
+                key: const Key(_nextBackground),
+              ),
+            ),
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                child: Container(
+                  color: Colors.black.withOpacity(0.0),
+                ),
+              ),
+            ),
+            buildTextContent(),
+          ],
         ),
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0), // 블러 강도 조절
-          child: Container(
-            color: Colors.black.withOpacity(0.0), // 배경 색 투명도 조정
+      ),
+    );
+  }
+
+  Widget buildTextContent() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            '우리가 함께한 지',
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.white,
+              fontFamily: 'jalnan',
+            ),
           ),
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                '우리가 함께한 지',
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.white,
-                  fontFamily: 'jalnan', // 폰트 적용
-                ),
-              ),
-              const SizedBox(height: 10), // 공간 추가
-              Text(
-                '$dDayText일',
-                style: const TextStyle(
-                  fontSize: 50,
-                  color: Colors.white,
-                  fontFamily: 'jalnan', // 폰트 적용
-                ),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                '${anniversary.year}.${anniversary.month.toString().padLeft(2, '0')}.${anniversary.day.toString().padLeft(2, '0')}.',
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                  fontFamily: 'Ultra', // 폰트 적용
-                ),
-              ),
-            ],
+          const SizedBox(height: 10),
+          Text(
+            '$dDayText일',
+            style: const TextStyle(
+              fontSize: 50,
+              color: Colors.white,
+              fontFamily: 'jalnan',
+            ),
           ),
-        ),
-      ]),
+          const SizedBox(height: 5),
+          Text(
+            '${anniversary.year}.${anniversary.month.toString().padLeft(2, '0')}.${anniversary.day.toString().padLeft(2, '0')}',
+            style: const TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontFamily: 'Ultra',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
